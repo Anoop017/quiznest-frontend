3ea970550cfd434a803ec2133719a7458bf8f19b
Anoop017 <anoopsnair1123@gmail.com>
2025-10-19 22:42:59 +0530

    Fixed issue with local host

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://quiznest-backend.onrender.com/api';
 
 export const apiRequest = async (endpoint, options = {}) => {
   const url = `${API_BASE_URL}${endpoint}`;
